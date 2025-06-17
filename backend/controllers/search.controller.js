6946607209ad4f371c931d95b2442caca8979fc3
manishbisht2000 <manishbisht2000@gmail.com>
2025-06-17 22:53:21 +0530

    Added search functions

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,85 @@
+import { User } from "../models/user.model.js"
+import { fetchFromTMDB } from "../services/tmdb.service.js"
+
+export async function searchPerson(req, res) {
+    // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
+    const {query} = req.params
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if(response.results.length===0) {
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "person",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchPerson controller:", error.message)
+        res.status(500).json({success:false, message:"Internal sever error"})
+    }
+}
+export async function searchMovie(req, res) {
+    const {query} = req.params
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if(response.results.length===0) {
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].title,
+                    searchType: "Movie",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchMovie controller:", error.message)
+        res.status(500).json({success:false, message:"Internal sever error"})
+    }
+}
+export async function searchTv(req, res) {
+    const {query} = req.params
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if(response.results.length===0) {
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "tv",
+                    createdAt: new Date(),
+                }
+            }
+        })
+
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchTv controller:", error.message)
+        res.status(500).json({success:false, message:"Internal sever error"})
+    }
+}
---
+++ backend/routes/search.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { searchMovie, searchPerson, searchTv } from "../controllers/search.controller.js"
+
+const router = express.Router()
+
+
+router.get("/person/:query", searchPerson)
+router.get("/movie/:query", searchMovie)
+router.get("/tv/:query", searchTv)
+
+export default router
