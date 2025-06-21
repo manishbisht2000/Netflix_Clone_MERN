4b0895fd861114eeb3be332bfce4d615d79ab932
manishbisht2000 <manishbisht2000@gmail.com>
2025-06-21 23:04:09 +0530

    Auth screen UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/home/HomePage"
+import Footer from "./components/Footer"
 
 
 function App() {
 
   return (
+    <>
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/signup" element={<SignupPage/>} />
     </Routes>
+    <Footer />
+    </>
   )
 }
 
