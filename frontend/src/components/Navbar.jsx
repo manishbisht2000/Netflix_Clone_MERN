99076b5d50a6ca597ce9a0734272750e8a087ede
manishbisht2000 <manishbisht2000@gmail.com>
2025-06-28 01:26:55 +0530

    Dynamic content in Hero section (HomePage)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import { LogOut, Menu, Search } from 'lucide-react'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthStore } from '../store/authUser'
+import { useContentStore } from '../store/content'
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
     const {user, logout}= useAuthStore()
 
     const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen)
+
+    const { setContentType} = useContentStore()
 
   return (
     <header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
@@ -18,10 +21,10 @@ const Navbar = () => {
 
             {/* Desktop navbar items */}
             <div className='hidden sm:flex gap-2 items-center'>
-                <Link to="/" className='hover:underline'>
+                <Link to="/" className='hover:underline' onClick={() => setContentType("movie")}>
                     Movies
                 </Link>
-                <Link to="/" className='hover-underline'>
+                <Link to="/" className='hover-underline' onClick={() => setContentType("tv")}>
                     Tv Shows
                 </Link>
                 <Link to="/history" className='hover-underline'>
--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -3,15 +3,29 @@ import React from 'react'
 import Navbar from '../../components/Navbar'
 import { Link } from 'react-router-dom'
 import { Info, Play } from 'lucide-react'
+import useGetTrendingContent from '../../hooks/useGetTrendingContent'
+import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants'
 
 const HomeScreen = () => {
+  const {trendingContent} = useGetTrendingContent()
+  console.log("trendingContent", trendingContent)
+
+  if(!trendingContent) return (
+    <div className='h-screen text-white relative'>
+      <Navbar />
+      <div 
+        className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer'
+      />
+    </div>
+  )
   
   return (
     <>
     <div className='relative h-screen text-white'>
       <Navbar />
 
-      <img src='extraction.jpg' alt='Hero img'
+      <img src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path} 
+        alt='Hero img'
         className='absolute top-0 left-0 w-full h-full object-cover -z-50'
       />
 
@@ -20,17 +34,31 @@ const HomeScreen = () => {
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32'>
         <div className='bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10' />
         <div className='max-w-2xl'>
-          <h1 className='mt-4 text-6xl font-extrabold text-balance'>Extraction</h1>
-          <p className='mt-2 text-lg'>2014 | 18+</p>
-          <p className='mt-4 text-lg'>Chris Hemsworth stars in this nonstop action-thriller with Rudraksh Jsiswal, Reandeep Hooda and Golshifteh Farahani</p>
+          <h1 className='mt-4 text-6xl font-extrabold text-balance'>
+            {trendingContent?.title || trendingContent?.name}
+          </h1>
+          <p className='mt-2 text-lg'>
+            {
+              trendingContent?.release_date?.split("-")[0] ||
+                trendingContent?.first_air_date.split("-")[0]}{" "}
+             | {trendingContent?.adult ? "18+" : "PG-13"}
+             </p>
+          <p className='mt-4 text-lg'>
+            { 
+              trendingContent?.overview.length > 200
+              ? trendingContent?.overview.slice(0, 200) + "..."
+              : trendingContent?.overview
+            } 
+            
+          </p>
         </div>
 
         <div className='flex mt-8'>
-          <Link to="/watch/123" className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center'>
+          <Link to={`/watch/${trendingContent?.id}`} className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center'>
           <Play className='size-6 mr-2 fill-black'></Play>
           Play
           </Link>
-          <Link to="/watch/123" className='bg-gray-500 text-white py-2 px-4 rounded flex items-center'>
+          <Link to={`/watch/${trendingContent?.id}`} className='bg-gray-500 text-white py-2 px-4 rounded flex items-center'>
           <Info className='size-6 mr-2'></Info>
           More Info
           </Link>
