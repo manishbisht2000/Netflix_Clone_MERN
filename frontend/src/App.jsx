eb1d68e4b18312e454625024e29cb8f4e5a11691
manishbisht2000 <manishbisht2000@gmail.com>
2025-07-02 21:34:02 +0530

    Search page for movies, tv shows & person

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useAuthStore } from "./store/authUser"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./pages/home/WatchPage"
+import SearchPage from "./pages/SearchPage"
 
 
 function App() {
@@ -35,6 +36,9 @@ function App() {
       <Route path="/login" element={!user? <LoginPage/> : <Navigate to={"/"} />} />
       <Route path="/signup" element={!user? <SignupPage/> : <Navigate to={"/"} />} />
       <Route path="/watch/:id" element={user? <WatchPage/> : <Navigate to={"/login"} />} />
+
+      <Route path="/search" element={user? <SearchPage/> : <Navigate to={"/login"} />} />
+    
     </Routes>
     <Footer />
 
