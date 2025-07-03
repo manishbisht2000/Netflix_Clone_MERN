e67557da1be674962461c8d613f7ca7a73d23457
manishbisht2000 <manishbisht2000@gmail.com>
2025-07-03 21:26:24 +0530

    Error 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { Loader } from "lucide-react"
 import WatchPage from "./pages/home/WatchPage"
 import SearchPage from "./pages/SearchPage"
 import SearchHistoryPage from "./pages/SearchHistoryPage"
+import NotFoundPage from "./pages/404"
 
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
 
       <Route path="/search" element={user? <SearchPage/> : <Navigate to={"/login"} />} />
       <Route path="/history" element={user? <SearchHistoryPage/> : <Navigate to={"/login"} />} />
+      <Route path="/*" element={<NotFoundPage />} />
     
     </Routes>
     <Footer />
